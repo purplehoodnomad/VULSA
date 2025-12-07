25e37309168e305fe9c37cb29f2f20f5371325d3
purplehoodnomad <dosex@silently.wow>
2025-12-07 23:06:45 +0500

    Add: base recreate upon server start

--- src/app.py
+++ src/app.py
@@ -20,7 +20,9 @@ async def lifespan(app: FastAPI):
 
     # --- startup: создаём таблицы один раз (идемпотентно) ---
     async with sessionmanager.connect() as connection:
+        await sessionmanager.drop_all(connection)
         await sessionmanager.create_all(connection)
+        
 
     try:
         yield
